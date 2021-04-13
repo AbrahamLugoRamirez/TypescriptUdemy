aa281720bc6359e911ed6a56e09f3d8ef4cbed53
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2021-04-13 11:33:29 -0500

    first commit

---
+++ seccion2/boolean.ts
@@ -0,0 +1,13 @@
+//let declara la variable
+//nombre de la variable
+//: ripo de dato -- boolean
+// = asignacion.
+//izipizi
+
+let si:boolean = true
+let no:boolean = false
+
+//puedo cambiar el valor de la variable (let)
+
+si = false
+no = true
---
+++ seccion2/tipado.ts
@@ -0,0 +1,4 @@
+var numero = 5
+let nombre:string = 'Abraham Lugo' //Su valor es modificable
+nombre = 'Abraham Lugo Ramirez'
+const edad:number = 22 //Su valor no se puede modificar
